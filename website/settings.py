e1c948922c0f95652a6ac865edcea5b11582ff8e
8686-code <140550672+8686-code@users.noreply.github.com>
2023-08-12 02:34:17 +0530

    commit

--- website/settings.py
+++ website/settings.py
@@ -130,12 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 ############################### Added manually ###################################
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # MESSAGE_TAGS = {messages.INFO: "",50: "critical",}
 
