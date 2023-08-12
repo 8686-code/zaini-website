ba70a6e6d6b166d1c301d419153c66f193e7ed0f
8686-code <140550672+8686-code@users.noreply.github.com>
2023-08-12 20:40:14 +0530

    commmit

--- website/settings.py
+++ website/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'business.apps.BusinessConfig',
     'django.contrib.admin',
+    'business.apps.BusinessConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = 'staticfiles_build'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 ############################### Added manually ###################################
